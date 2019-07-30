060c381b8518ed219f998df39cc7fa61ec5b6e7c
Shahrukh Saba <shahrukhsaba@gmail.com>
2019-07-30 23:06:49 +0800

    Update create-nursery.component.ts

--- create-nursery.component.ts
+++ create-nursery.component.ts
@@ -1,115 +1,75 @@
-<div class="modal-body overflow-y-auto h-100">
-<div>
-  <h1 class="text-muted">CREATE NURSERY<small> - let's recreate mother nature!</small></h1>
-</div>
-<br>
-<div>
-  <h5>Nursery Proposal Site Location:</h5>
-</div>
-<app-map></app-map>
-<br>
-<div class="container h-100">
-  <div>
-      <div id="panel">
-          <div id="color-palette"></div>
-          <div class="row">
-              <div class="col-lg-6">
-                  <fieldset class="form-group">
-                      SELECTION AREA:&nbsp;<input type="text" id="area" [(ngModel)]="selectionArea">
-                  </fieldset>
-                  <button class="btn btn-secondary" (click)="setAlpha()">set alpha</button>&nbsp;
-                  <button class="btn btn-secondary" (click)="setBeta()">set beta</button>&nbsp;
-                  <button class="btn btn-secondary" (click)="setCharlie()">set charlie</button>
-              </div>
-              <div class="col-lg-6">
-                  <button class="btn btn-secondary" id="calculate" (click)="calcar()">Calculate area</button>&nbsp;
-                  <button class="btn btn-secondary" id="delete-button" (click)="deleteSelectedShape()">Delete Selected
-                  Shape</button>  
-            </div>
-          </div>
-      <br>
-      <h5>Nursery Details:</h5>
-      <form [formGroup]="nurseryForm">
-      <div class="row">
-              <div class="col-lg-6">
-              <fieldset class="form-group">
-                      <label>Alpha threshold:</label>
-                      <input class="form-control" id="alpha_area" formControlName="alpha_threshold">
-              </fieldset>
+import { Component, OnInit } from '@angular/core';
+import { MapComponent } from '../map/map.component';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MapLoaderService } from 'src/app/services/shared/map-loader.service';
+import { NurseryService } from 'src/app/services/nursery.service';
 
-              <fieldset class="form-group">
-                      <label>Beta threshold:</label>
-                      <input class="form-control"  id="beta_area" formControlName="beta_threshold" >
-              </fieldset>
+@Component({
+  selector: 'app-create-nursery',
+  templateUrl: './create-nursery.component.html',
+  styleUrls: ['./create-nursery.component.scss']
+})
+export class CreateNurseryComponent extends MapComponent implements OnInit {
 
-              <fieldset class="form-group">
-                      <label>Charlie threshold:</label>
-                      <input class="form-control"  id="charlie_area" formControlName="charlie_threshold" >
-              </fieldset>
+  nurseryForm = new FormGroup({
+    nurseryRegistrationNumber: new FormControl(),
+    firstName: new FormControl(),
+    familyName: new FormControl(),
+    nurseryName: new FormControl(),
+    comName: new FormControl(),
+    nurseryDescription: new FormControl(),
+    address: new FormControl(),
+    contactNumber: new FormControl(),
+    email: new FormControl(),
+    alpha_threshold: new FormControl(),
+    beta_threshold: new FormControl(),
+    charlie_threshold: new FormControl(),
+    donationNeeded: new FormControl()
+  });
+  error: string = null;
 
-              <fieldset class="form-group">
-                  <label>Description:</label>
-                  <textarea class="form-control" formControlName='nurseryDescription' rows="9"></textarea>
-              </fieldset>
-    
-              <fieldset class="form-group">
-                      <label>Address:</label>
-                      <textarea class="form-control" formControlName='address' rows="5"></textarea>
-              </fieldset>
-            </div>
+  constructor(
+    protected mapLoaderService: MapLoaderService,
+    private nurseryService: NurseryService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) {
+    super(mapLoaderService);
+  }
 
-            <div class="col-lg-6">
+  ngOnInit() {
+  }
 
-                <fieldset class="form-group">
-                    <label>Nursery Registration Number:</label>
-                    <input class="form-control" formControlName="nurseryRegistrationNumber" placeholder="Registration Number">
-                </fieldset>
-  
-                <fieldset class="form-group">
-                  <label>Nursery Site Name:</label>
-                  <input class="form-control" formControlName="nurseryName" placeholder="Nursery Site Name">
-              </fieldset>
-              
+  createNursery() {
+    const nurseryData = this.nurseryForm.value;
+    this.nurseryService.createNursery(nurseryData).subscribe(() => {
+      this.router.navigate(['ngolist']);
+      return;
+    },
+      err => {
+        console.error(err);
+      }
+    );
+  }
 
-          <fieldset class="form-group">
-              <label>Contact Number:</label>
-              <input class="form-control" formControlName="contactNumber" placeholder="Nursery contact number">
-          </fieldset>
-      
-          <fieldset class="form-group">
-                  <label>Email:</label>
-                  <input class="form-control" formControlName="email" placeholder="Email">
-          </fieldset>
+  public setAlpha() {
+    this.nurseryForm.patchValue({
+      alpha_threshold: this.selectionArea
+    });
+  }
 
-                <fieldset class="form-group">
-                        <label>Donation Needed:</label>
-                        <input class="form-control" formControlName="donationNeeded" placeholder="Donation needed">
-                </fieldset>
+  public setBeta() {
+    this.nurseryForm.patchValue({
+      beta_threshold: this.selectionArea
+    });
+  }
 
-              <fieldset class="form-group">
-                  <label>Nursery Administrator First Name:</label>
-                  <input class="form-control" formControlName="firstName" placeholder="First Name">
-              </fieldset>
+  public setCharlie() {
+    this.nurseryForm.patchValue({
+      charlie_threshold: this.selectionArea
+    });
+  }
 
-              <fieldset class="form-group">
-                  <label>Nursery Administrator Family Name:</label>
-                  <input class="form-control" formControlName="familyName" placeholder="Family Name">
-              </fieldset>
 
-              <fieldset class="form-group">
-                  <label>Community name:</label>
-                  <input class="form-control" formControlName="comName" placeholder="Community name">
-              </fieldset>
-
-              <!-- <fieldset class="form-group">
-                            <label for="exampleInputFile">Upload Site Photos:</label>
-                            <input type="file" class="form-control-file" id="exampleInputFile">
-                        </fieldset> -->            
-              </div>
-            </div>
-              <button class="btn btn-secondary" (click)="createNursery()">Create Nursery</button>&nbsp;
-              <button type="reset" class="btn btn-secondary">Reset Fields</button>
-          </form>
-  </div>
-</div>
-</div>
+}
